11110a882a87c40c10b8a1c91891bdf5579542c6
Jakob Schwendner <jakob.schwendner@gmail.com>
2012-02-28 21:54:48 +0100

    moved BodyContactState into its own hpp file

--- src/ContactModel.hpp
+++ src/ContactModel.hpp
@@ -7,36 +7,10 @@
 #include <terrain_estimator/TerrainConfiguration.hpp>
 #include <envire/maps/MLSGrid.hpp>
 #include <asguard/Configuration.hpp>
+#include <eslam/ContactState.hpp>
 
 namespace eslam
 {
-
-struct BodyContactPoint
-{
-    /** @brief position in body frame */
-    base::Vector3d position;
-
-    /** @brief contact probability in the interval between 0 and 1.0, or NaN if unknown */
-    double contact;
-
-    /** @brief slip distance of contact point */
-    double slip;
-
-    /** @brief contact group id, or -1 if not part of a group */
-    int groupId;
-};
-
-struct BodyContactState
-{
-    /** @brief timestamp */
-    base::Time time;
-    
-    /** @brief vector of all potential contact points of the body and their states.
-     * 
-     * The order of the contact points in the vector should not change over data items.
-     */
-    std::vector<BodyContactPoint> points;
-};
 
 /** 
  * Contactmodel class that relates the kinematic configuration of a robot with
