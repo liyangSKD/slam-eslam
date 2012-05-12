6ef75b35bff8a6a02d29d88d1bbfff5c662ce70f
Jakob Schwendner <jakob.schwendner@gmail.com>
2012-05-12 13:46:17 +0200

    testMap: removed planeFitting attempt and added simple odometry
    - only works when no twistAngle or body to world orientation
      other than identity is involved.

--- test/testMap.cpp
+++ test/testMap.cpp
@@ -19,6 +19,7 @@ struct MapTest
     asguard::Configuration asguardConfig;
     asguard::BodyState bodyState;
     odometry::FootContact odometry;
+    Eigen::Affine3d body2world;
 
     MapTest()
 	: odometry( odometry::Configuration() )
@@ -33,6 +34,8 @@ struct MapTest
 
 	grid = new MLSGrid( 100, 100, 0.05, 0.05, -2.5, -2.5 );
 	env->setFrameNode( grid, env->getRootNode() );
+
+	body2world = Eigen::Affine3d::Identity();
     }
 
     void run()
@@ -50,7 +53,13 @@ struct MapTest
 	for(int j=0;j<4;j++)
 	    bodyState.wheelPos[j] += 0.1;
 
+	odometry::BodyContactState bcs;
+	asguardConfig.setContactState( bodyState, bcs );
+	odometry.update( bcs, Eigen::Quaterniond::Identity() );
+	body2world = body2world * odometry.getPoseDelta().toTransform();
+
 	aviz.updateData( bodyState );
+	aviz.updateTransform( body2world );
 
 	// grid update
 	MLSGrid::Position p;
@@ -64,53 +73,6 @@ struct MapTest
 	}
     }
 
-    void fitPlane( asguard::BodyState &body_state )
-    {
-	// find the twist angle, such that we can fit a plane
-	// to the points
-	float eps = 1e-3, error = 1e3;
-	float step = 0.1;
-	size_t contact_idx[4] = {0,0,0,0};
-
-	while( error > eps )
-	{
-	    Eigen::Vector3d p1, p2, p3, p4;
-	    p1 = asguardConfig.getFootPosition( body_state, asguard::wheelIdxEnum(0), contact_idx[0] );
-	    p2 = asguardConfig.getFootPosition( body_state, asguard::wheelIdxEnum(1), contact_idx[1] );
-	    p3 = asguardConfig.getFootPosition( body_state, asguard::wheelIdxEnum(2), contact_idx[2] );
-	    p4 = asguardConfig.getFootPosition( body_state, asguard::wheelIdxEnum(3), contact_idx[3] );
-
-	    Eigen::Matrix3d r;
-	    r.row(0) = p2 - p1;
-	    r.row(1) = (p3 - p1).cross( r.row(0) );
-	    r.row(2) = r.row(0).cross( r.row(1) );
-	    Eigen::Affine3d t;
-	    t.linear() = r;
-	    t.translation() = p1;
-
-	    for(int wheel_idx = 0; wheel_idx < 4; wheel_idx++)
-	    {
-		for (unsigned int foot_idx = 0; foot_idx < 5; ++foot_idx)
-		{
-		    Eigen::Vector3d p = 
-			asguardConfig.getFootPosition(body_state, asguard::wheelIdxEnum(wheel_idx), foot_idx);
-
-		    if( (t.inverse() * p).z() < 0 )
-		    {
-			if( wheel_idx == 3 )
-			{
-
-			}
-			else 
-			{
-			    contact_idx[wheel_idx] = (contact_idx[wheel_idx]+1) % 4;
-			    break;
-			}
-		    }
-		}
-	    }
-	}
-    }
 };
 
 int main( int argc, char **argv )
